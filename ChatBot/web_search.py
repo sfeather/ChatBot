3363cbad3e121abb4ff706a4224abf235b7d2503
sfeather <stefanpana00@gmail.com>
2021-02-23 00:00:32 +0200

    Added Covid-19 option for news about a country.

--- ChatBot/web_search.py
+++ ChatBot/web_search.py
@@ -1,4 +1,5 @@
 import webbrowser as wb
+import covid_scrap as cs
 
 
 def search_on_web(keyword, input_string):
@@ -15,4 +16,9 @@ def search_on_web(keyword, input_string):
         url = 'https://www.google.com/search?client=firefox-b-d&q='
         wb.get().open_new(url + input_string.split(' ', 1)[1])
         response_text = 'Opened a new Google tab searching for ' + input_string.split(' ', 1)[1]
+    if any(x in keyword for x in ['covid', 'Covid', 'covid-19', 'Covid-19', 'coronavirus', 'corona', 'Coronavirus',
+                                  'Corona', 'CoronaVirus']):
+        string = cs.scrap(input_string.split(' ', 1)[1])
+        response_text = 'Covid-19 fresh news (24h) - ' + string.split('[', 1)[0]
+
     return response_text
