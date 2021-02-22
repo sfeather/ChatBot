354f62967a0ec0baa4552cad48fcaf2b1cb0597c
sfeather <stefanpana00@gmail.com>
2021-02-22 23:00:53 +0200

    Added language.

--- ChatBot/speech_rec.py
+++ ChatBot/speech_rec.py
@@ -1,13 +1,13 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
 
-
-with sr.Microphone(device_index=12) as source:
-    print('Speak Anything: ')
-    audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        print('You said: {}'.format(text))
-    except:
-        print('Sorry, we could not recognize your voice')
+def speak_func(lang):
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=12) as source:
+        print('Speak Anything: ')
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language=lang)
+            return text
+        except Exception:
+            return 'Sorry, we could not recognize your voice'
