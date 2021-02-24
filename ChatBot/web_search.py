47a614630597fac9f111d677234518879adb33d9
sfeather <stefanpana00@gmail.com>
2021-02-24 14:37:16 +0200

    More efficient comparison using .lower method.

--- ChatBot/web_search.py
+++ ChatBot/web_search.py
@@ -4,7 +4,8 @@ import covid_scrap as cs
 
 def search_on_web(keyword, input_string):
     response_text = ''
-    if any(x in keyword for x in ['YouTube', 'youtube', 'Youtube', 'yt']):
+
+    if any(x in keyword.lower() for x in ['youtube', 'yt']):
         if len(input_string.split(' ', 1)) != 1:
             url = 'https://www.youtube.com/results?search_query='
             wb.get().open_new(url + input_string.split(' ', 1)[1])
@@ -12,7 +13,7 @@ def search_on_web(keyword, input_string):
         else:
             response_text = 'ChatBot: You are using a wrong command syntax\n' \
                             'The command syntax should be: command + keyword'
-    if any(x in keyword for x in ['Wikipedia', 'wiki', 'wikipedia']):
+    if any(x in keyword.lower() for x in ['wiki', 'wikipedia']):
         if len(input_string.split(' ', 1)) != 1:
             url = 'https://en.wikipedia.org/wiki/'
             wb.get().open_new(url + input_string.split(' ', 1)[1])
@@ -20,7 +21,7 @@ def search_on_web(keyword, input_string):
         else:
             response_text = 'ChatBot: You are using a wrong command syntax\n' \
                             'The command syntax should be: command + keyword'
-    if any(x in keyword for x in ['Google', 'google', 'search', 'Search']):
+    if any(x in keyword.lower() for x in ['google', 'search', 'find']):
         if len(input_string.split(' ', 1)) != 1:
             url = 'https://www.google.com/search?client=firefox-b-d&q='
             wb.get().open_new(url + input_string.split(' ', 1)[1])
@@ -28,8 +29,7 @@ def search_on_web(keyword, input_string):
         else:
             response_text = 'ChatBot: You are using a wrong command syntax\n' \
                             'The command syntax should be: command + keyword'
-    if any(x in keyword for x in ['covid', 'Covid', 'covid-19', 'Covid-19', 'coronavirus', 'corona', 'Coronavirus',
-                                  'Corona', 'CoronaVirus']):
+    if any(x in keyword.lower() for x in ['covid', 'covid-19', 'coronavirus', 'corona']):
         if len(input_string.split(' ', 1)) != 1:
             string = cs.scrap(input_string.split(' ', 1)[1])
             response_text = 'Covid-19 fresh news (24h) - ' + string.split('[', 1)[0]
